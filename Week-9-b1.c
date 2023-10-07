2ec005cd8c0aeb0bcc75ef3e317ad4bcdfb7a682
Ali Khan <alikhan37544@gmail.com>
2023-10-08 00:54:50 +0530

    changed up code

--- Week-9-b1.c
+++ Week-9-b1.c
@@ -1,70 +1,75 @@
-// Implementing BFS in graphs
-
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define MAX_SIZE 100
 
-int graph[MAX_SIZE][MAX_SIZE];
-bool visited[MAX_SIZE];
 int queue[MAX_SIZE];
 int front = -1, rear = -1;
 
-void enqueue(int vertex) {
-    if (rear == MAX_SIZE - 1) {
-        printf("Queue is full. Cannot enqueue more elements.\n");
+// Function to check if the queue is empty
+bool isEmpty() {
+    return front == -1;
+}
+
+// Function to check if the queue is full
+bool isFull() {
+    return rear == MAX_SIZE - 1;
+}
+
+// Function to enqueue a customer into the queue
+void enqueue(int customer) {
+    if (isFull()) {
+        printf("Queue is full. Cannot enqueue more customers.\n");
     } else {
         if (front == -1) {
             front = 0;
         }
-        queue[++rear] = vertex;
+        queue[++rear] = customer;
+        printf("Customer %d joined the queue.\n", customer);
     }
 }
 
-int dequeue() {
-    if (front == -1) {
-        printf("Queue is empty. Cannot dequeue.\n");
-        return -1;
+// Function to dequeue a customer from the queue
+void dequeue() {
+    if (isEmpty()) {
+        printf("Queue is empty. No customer to dequeue.\n");
     } else {
-        int vertex = queue[front++];
+        int customer = queue[front++];
+        printf("Customer %d checked out and left the queue.\n", customer);
         if (front > rear) {
             front = rear = -1;
-        }
-        return vertex;
-    }
-}
-
-void bfs(int start, int vertices) {
-    visited[start] = true;
-    printf("BFS Traversal: %d ", start);
-    enqueue(start);
-    while (front != -1) {
-        int current = dequeue();
-        for (int i = 0; i < vertices; i++) {
-            if (graph[current][i] == 1 && !visited[i]) {
-                visited[i] = true;
-                printf("%d ", i);
-                enqueue(i);
-            }
         }
     }
 }
 
 int main() {
-    int vertices;
-    printf("Enter the number of vertices in the graph: ");
-    scanf("%d", &vertices);
-    printf("Enter the adjacency matrix:\n");
-    for (int i = 0; i < vertices; i++) {
-        for (int j = 0; j < vertices; j++) {
-            scanf("%d", &graph[i][j]);
+    int choice, customer;
+
+    while (true) {
+        printf("--- Supermarket Checkout System Menu ---\n");
+        printf("1. Enqueue Customer\n");
+        printf("2. Dequeue Customer\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter customer number: ");
+                scanf("%d", &customer);
+                enqueue(customer);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                printf("Exiting the supermarket checkout system.\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
         }
     }
-    int start;
-    printf("Enter the starting vertex for BFS: ");
-    scanf("%d", &start);
-    bfs(start, vertices);
-    printf("\n");
+
     return 0;
 }
